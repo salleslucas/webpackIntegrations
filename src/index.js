130ff2f6647ae8747e3e786d5ef554b97e785158
Lucas Almeida Salles <lucas.almeida@g.globo>
2025-01-21 22:14:31 -0300

    integracao com fonts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+// import fonts
+import './fonts.css';
+
+// import jQuery
 import $ from 'jquery';
+
 
 const body = $('body');
 const p = $('<p></p>').text('jQuery is working!').css('color', 'red');
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,26 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    mode: 'development',
     output: {
             filename: 'bundle.js',
             path: path.resolve(__dirname, './dist')
     },
+    module:{
+        rules:[
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader', 'css-loader'
+                ]
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot|otf)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]', // Salva as fontes na pasta "fonts"
+                },
+            },
+        ]
+    }
 }
